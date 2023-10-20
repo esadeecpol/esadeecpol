fe2c263e3d5f377bd4e901430032dbf53224ed00
Javi Martinez <javier.martinez.snt@gmail.com>
2023-10-20 20:01:14 +0200

    Update saveplots

---
+++ R/Save_plot.R
@@ -0,0 +1,34 @@
+#' @title Funciones de exportación de graficos de EsadeEcPol
+#' @description Las funciones de este fichero sirven para hacer mas rapido la exportacion de las visualizaciones de Esade, ya que dejamos valores por defecot
+#' 
+#' @param plot componente al que asignamos la figura de ggplot
+#' @param filename Nombre que se le quiere dar al archivo, incluyendo ruta
+#' @param width Ancho de la figura
+#' @param height Alto de la figura
+#' @param unit Unidades de medida
+#' @param dpi Densidad de pixeles por pulgada
+
+save_to_png <- function(plot, filename = "", width = 18, height = 12, unit = "cm", dpi = 300) {
+  ggsave(
+    plot = plot,
+    filename = filename,
+    device = 'png',
+    width = width,
+    height = height,
+    unit = unit,
+    dpi = dpi
+  )
+}
+
+# Función para guardar gráficos en PDF
+save_to_svg <- function(plot, filename = "", width = 18, height = 12, unit = "cm", dpi = 300) {
+  ggsave(
+    plot = plot,
+    filename = filename,
+    device = 'svg',
+    width = width,
+    height = height,
+    unit = unit,
+    dpi = dpi
+  )
+}
