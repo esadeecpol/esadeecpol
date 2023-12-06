6bf445c19d4bae87ce67ecbd841a9adc684d4b48
Javi Martinez <76729762+javimartzs@users.noreply.github.com>
2023-12-06 09:32:00 +0100

    Update Fonts.R

--- R/Fonts.R
+++ R/Fonts.R
@@ -5,22 +5,32 @@
 
 fonts_ecpol <- function() {
   library(systemfonts)
-  
+  library(dplyr)
+  library(stringr)
+
   mabry <- system_fonts() %>%
     filter(family == "Mabry Pro") %>%
     transmute(family, style, file = str_extract(path, "[\\w-]+\\.ttf$"))
-  
+
   mabry_light_path <- system_fonts() %>%
     filter(family == "Mabry Pro", style == "Light") %>%
     pull(path)
   
   assign("mabry_light", mabry_light_path, envir = .GlobalEnv)
   systemfonts::register_font(name = "Mabry Pro Light", plain = mabry_light_path)
-  
+
   mabry_med_path <- system_fonts() %>%
     filter(family == "Mabry Pro", style == "Medium") %>%
     pull(path)
   
   assign("mabry_medium", mabry_med_path, envir = .GlobalEnv)
   systemfonts::register_font(name = "Mabry Pro Medium", plain = mabry_med_path)
+
+  # Añadir Mabry Pro Light Italic
+  mabry_light_italic_path <- system_fonts() %>%
+    filter(family == "Mabry Pro", style == "Light Italic") %>%
+    pull(path)
+
+  assign("mabry_light_italic", mabry_light_italic_path, envir = .GlobalEnv)
+  systemfonts::register_font(name = "Mabry Pro Light Italic", italic = mabry_light_italic_path)
 }
