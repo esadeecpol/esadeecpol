2b83beae63d18024c923c34ad0aea68b21277084
Javi Martinez <javier.martinez.snt@gmail.com>
2024-04-28 11:48:45 +0200

    update colors function

--- R/Print_colors.R
+++ R/Print_colors.R
@@ -21,3 +21,28 @@ print_colors <- function() {
 
 print_colors()
 
+
+
+#' @title Lista de colores EsadeEcPol para usar en ggploot
+#' @description Esta función sirve para usar los colores en español en vez de en hexa
+#' 
+#' 
+#' @examples 
+#' colors()
+#' 
+#' @return vector con el nombre y el hexadecimal que representa
+#' @export
+#' 
+
+names <- c('azul', 'azul_esade', 'rojo', 'verde', 'amarillo', 'magenta', 'fucsia', 'morado',
+'naranja', 'gris_oscuro', 'negro')
+
+hexas <- c('#225E9C', '#030B3D','#E05656','#00A47D', '#EFB43F', '#952EA0', '#E6007E', '#4B2991',  '#F27200', '#404040', '#1a1a1a')
+
+# FUncion para crear el vector de colores
+color_vector <- function(names, hexas) {
+    colors <- setNames(hexas, names)
+    return(colors)
+}
+
+color_vector <- color_vector(name, hexa)
